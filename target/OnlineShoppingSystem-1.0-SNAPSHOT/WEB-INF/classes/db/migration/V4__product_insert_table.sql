2bff3e67e664744ed75aed276a75c5018f5fa53d
diyastursunkali <dias.tursunkali@gmail.com>
2021-04-26 08:31:06 +0600

    1

---
+++ target/OnlineShoppingSystem-1.0-SNAPSHOT/WEB-INF/classes/db/migration/V4__product_insert_table.sql
@@ -0,0 +1,4 @@
+insert into products
+values (1, 'iPhone 12', 450000, 50),
+       (2, 'iPhone 11', 350000, 40),
+       (3, 'iPhone X', 300000, 30)
---
+++ target/classes/db/migration/V2__product_creat_table.sql
@@ -0,0 +1,7 @@
+create table products
+(
+    id       integer,
+    name     varchar,
+    price    integer,
+    quantity integer
+);
